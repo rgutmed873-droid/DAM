a61e42d3565832f1ae361d42b0923576d14aa8cd
rgutmed873-droid <rgutmed873@iesgrancapitan.org>
2025-11-25 20:04:02 +0100

    ejercicio matrices

--- src/Matrices/ejercicio1.java
+++ src/Matrices/ejercicio1.java
@@ -13,13 +13,35 @@ public class ejercicio1 {
         int [] [] matriz = new int [5][5];
 
 
-        tablero(matriz);
+        rellenar(matriz);
+        imprimirmatriz(matriz);
+        positivo(matriz);
+
     }
 
-    private static void tablero(int[][] matriz) {
+    private static void positivo(int[][] matriz) {
+        int positivos = 0;
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[0].length; j++) {
-                matriz[i][j] = numeroAleatorio(0,100);
+               matriz[i][j] = numeroAleatorio(-10,10);
+
+                if (matriz[i][j] >= 0){
+                    positivos++;
+
+                }
+            }
+            if (positivos == 25){
+                System.out.println("La matriz es positiva");
+            }else {
+                System.out.println("Hay numeros de ambos tipos");
+            }
+        }
+
+    }
+
+    private static void imprimirmatriz(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
 
                 System.out.print("|" + matriz[i][j] + "|");
             }
@@ -27,8 +49,19 @@ public class ejercicio1 {
         }
     }
 
+    private static void rellenar(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                matriz[i][j] = numeroAleatorio(-10,10);
+
+            }
+        }
+    }
+
     public static int numeroAleatorio(int min,int max){
         Random aleatorio = new Random();
         return aleatorio.nextInt(max-min+1)+min;
     }
+
+
 }
