2807141458e2f6c6e4954f392c9f1d54394f819b
rgutmed873-droid <rgutmed873@iesgrancapitan.org>
2025-12-12 20:09:16 +0100

    Pet shop avanzado y hecho el sexo animales y pedigree perro

--- src/Objetos/Petshop/Birds.java
+++ src/Objetos/Petshop/Birds.java
@@ -12,6 +12,22 @@ public class Birds extends Animals{
         this.type = type;
     }
 
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     @Override
     protected int convertidorEdad(int edad) {
         return super.convertidorEdad(edad);
